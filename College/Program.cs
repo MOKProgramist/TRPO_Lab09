e7c835d67716d602c009f67db04f97e15ecbd61b
Alex <gundorovsasha@gmail.com>
2022-12-12 14:46:12 +0300

    Добавьте файлы проекта.

---
+++ College/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DllLibrary;
+
+namespace College
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Specialty specialtyProgrammist = new Specialty(name: "09.02.07 Программист", countStudent: 25, studyPeriod: "3 года 10 месяцев");
+            // specialtyProgrammist.Show();
+            Specialty specialtyAgent = new Specialty(name: "Техническая поддержка", countStudent: 35, studyPeriod: "2 года 10 месяцев");
+            // specialtyAgent.Show();
+
+            Сollege college = new Сollege(name: "Первый МОК", address: "г. Москва, Староватутинский проезд, 6");
+            college.AddSpec(specialtyProgrammist);
+            college.AddSpec(specialtyAgent);
+
+            college.Show();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DllLibrary/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DllLibrary
+{
+    // класс колледж 
+    public class Сollege
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public List<Specialty> SpecialtyList = new List<Specialty>();
+        public Сollege(string name, string address)
+        {
+            Name = name;
+            Address = address;
+        }
+        public void Show()
+        {
+            if (SpecialtyList.Count == 0) Console.WriteLine($"Колледж - {Name} - расположен по адресу: {Address}. Специальности не указаны.");
+            else
+            {
+                Console.WriteLine($"Колледж - {Name} - расположен по адресу: {Address}.\nСписок специальностей: ");
+                foreach (Specialty specialty in SpecialtyList) specialty.Show();
+            }
+        }
+
+        // добавление специальнстей в колледж
+        public void AddSpec(Specialty specialty)
+        {
+            SpecialtyList.Add(specialty);
+            // Console.WriteLine("Добавлена новая специальность в колледж.");
+        }
+    }
+
+    // класс специальность
+    public class Specialty
+    {
+        public string Name {get; set; }
+        public int CountStudent { get; set; }
+
+        // период обучения
+        public string StudyPeriod { get; set; }
+
+        public Specialty(string name, int countStudent, string studyPeriod)
+        {
+            Name = name;
+            CountStudent = countStudent;
+            StudyPeriod = studyPeriod;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Специальность {Name} имеет срок обучения {StudyPeriod} и количество студентов {CountStudent}");
+        }
+    }
+}
